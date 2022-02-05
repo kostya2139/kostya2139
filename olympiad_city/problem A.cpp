5a6201b0f9899d96200fbf1e02598181cbc09490
kostya2139 <97086581+kostya2139@users.noreply.github.com>
2022-02-05 10:48:13 +0200

    Update problem A.cpp

--- olympiad_city/problem A.cpp
+++ olympiad_city/problem A.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    int n, m, w ,t;
+    cin>>n>>m>>w>>t;
+    int times_of_permission=1+t/w;
+    int n_students_who_are_in=min(n, m*times_of_permission);
+    cout<<n_students_who_are_in;
+    return 0;
+}
